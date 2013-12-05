77d7f36fb519604af2df34128b95f801faa6d52f
Ducastel <benoit_duc@live.fr>
2013-12-05 12:42:25 +0100

    modification arme et hache

--- src/consolefps/models/elements/Hache.java
+++ src/consolefps/models/elements/Hache.java
@@ -1,7 +1,18 @@
 package consolefps.models.elements;
 
+import consolefps.models.Position;
+
 public  class   Hache extends Arme {
 	
+public Hache(int x, int y) {
+		super(x, y);
+		
+	}
+
+	public Hache(Position position) {
+		super(position);
+	}
+
 @Override
 	public String afficher() { // affiche l'arme Hache sur la grille avec un h
         return "[h]";
