53a18851581bf41c615009bc0968d78394195e3e
Ducastel <benoit_duc@live.fr>
2013-12-05 16:53:12 +0100

    creation package arme + test Hache

--- src/consolefps/models/elements/arme/FusilAPompe.java
+++ src/consolefps/models/elements/arme/FusilAPompe.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package consolefps.models.elements;
+package consolefps.models.elements.arme;
 
 import consolefps.models.Position;
 
---
+++ test/consolefps/models/testHache.java
@@ -0,0 +1,18 @@
+package consolefps.models;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import consolefps.models.elements.arme.Hache;
+
+public class testHache {
+
+	@Test
+	public void testAfficherHache() {
+		 Hache h = new Hache(0,0);
+		 String s = h.afficher();
+		 assertEquals("[h]",s);
+	}
+
+}
