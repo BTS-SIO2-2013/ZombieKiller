96f23a02e4cc5c859199217d3bb974418db51c3b
Ferie <tristan.ferie@yahoo.fr>
2013-12-05 16:54:13 +0100

    creation test fusil a pompe

---
+++ test/consolefps/models/FusilAPompeTest.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package consolefps.models;
+
+import consolefps.models.elements.FusilAPompe;
+import consolefps.models.elements.Zombie;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+/**
+ *
+ * @author titou
+ */
+public class FusilAPompeTest {
+    
+    public FusilAPompeTest() {
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+
+    @Test
+    public void testAfficherFusilAPompe() {
+        //Arrange
+        FusilAPompe p = new FusilAPompe(1,2);
+        
+        //Act
+        String s = p.afficher();
+        
+        //Assert 
+        assertEquals("[p]", s);
+    }
+    
+}
