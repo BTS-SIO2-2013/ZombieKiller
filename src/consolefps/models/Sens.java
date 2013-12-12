06df137ab3dad27d767b0f0d1fd3899acbc1bf79
Yanick <yanick.servant@free.fr>
2013-12-12 12:22:21 +0100

    Commande ATTENDRE

--- src/consolefps/models/Sens.java
+++ src/consolefps/models/Sens.java
@@ -5,13 +5,14 @@
 package consolefps.models;
 
 /**
- *
+ * 
  * @author krilivye
  */
 public class Sens {
-    public static final Sens BAS = new Sens();
-    public static final Sens DROITE = new Sens();
-    public static final Sens HAUT = new Sens();
-    public static final Sens GAUCHE = new Sens();
-    
+	public static final Sens BAS = new Sens();
+	public static final Sens DROITE = new Sens();
+	public static final Sens HAUT = new Sens();
+	public static final Sens GAUCHE = new Sens();
+	public static final Sens ATTENDRE = new Sens();
+
 }
