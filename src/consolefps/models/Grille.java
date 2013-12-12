05ac559f146353c854938dfbb321a5b32210ed05
Xav <xav.defer@gmail.com>
2013-12-12 10:03:22 +0100

    MAJ 12/12/13 10:00

--- src/consolefps/models/Grille.java
+++ src/consolefps/models/Grille.java
@@ -4,131 +4,131 @@
  */
 package consolefps.models;
 
-import consolefps.models.elements.Elements;
-import consolefps.models.elements.decors.Decors;
-
 import java.util.ArrayList;
 import java.util.List;
 
+import consolefps.models.elements.Elements;
+import consolefps.models.elements.decors.Decors;
+
 /**
- *
+ * 
  * @author krilivye
  */
 public class Grille {
-    private int nbLignes;
-    private int nbColonnes;
-    private List<Elements> objets;
+	private int nbLignes;
+	private int nbColonnes;
+	private List<Elements> objets;
 
-    public Grille(int nbColonnes, int nbLignes) {
-        this();
-        this.nbLignes = nbLignes;
-        this.nbColonnes = nbColonnes;
-    }
-    
-    public int getNbLignes(){
-        return this.nbLignes;
-    }
-    public int getNbColonnes(){
-        return this.nbColonnes;
-    }
+	public Grille(final int nbColonnes, final int nbLignes) {
+		this();
+		this.nbLignes = nbLignes;
+		this.nbColonnes = nbColonnes;
+	}
 
-    public Grille() {
-        this.nbLignes = 5;
-        this.nbColonnes = 5;
-        this.objets = new ArrayList<Elements>();
-    }
+	public int getNbLignes() {
+		return this.nbLignes;
+	}
 
-    public String afficherGrille() {
-        String retour = this.entete();
-        
-        for (int y=0;y<nbLignes;y++){
-            retour += this.afficherLigne(y);
-        }
-        return retour;
-    }
+	public int getNbColonnes() {
+		return this.nbColonnes;
+	}
 
-    public void ajouterElement(Elements e){
-        this.objets.add(e);
-    }
+	public Grille() {
+		this.nbLignes = 5;
+		this.nbColonnes = 5;
+		this.objets = new ArrayList<>();
+	}
 
-    public String entete() {
-        String str = " ";
-        for (int i =0;i<nbColonnes; i++){
-           str+=" "+i+" ";
-        }
-        return str+"\n"; 
-    }
+	public String afficherGrille() {
+		String retour = entete();
 
-    public String caseVide() {
-        return "[ ]";
-    }
+		for (int y = 0; y < this.nbLignes; y++) {
+			retour += afficherLigne(y);
+		}
+		return retour;
+	}
 
-    public String afficherLigne(int i) {
-        String retour=""+i;
-        for (int x= 0; x<nbColonnes;x++){
-            Position courante = new Position(x, i);
-            
-            boolean aAfficher = false;
-            for (Elements objet : this.objets){
-                if (objet.getPosition().equals(courante)){
-                    retour += objet.afficher();
-                    aAfficher = true;
-                }
-            }
-            if (!aAfficher){
-                 retour += this.caseVide();   
-            }
-        }
-        return retour +"\n";
-    }
+	public void ajouterElement(final Elements e) {
+		this.objets.add(e);
+	}
 
-    public List<Elements> getElements() {
-        return this.objets;
-    }
+	public String entete() {
+		String str = " ";
+		for (int i = 0; i < this.nbColonnes; i++) {
+			str += " " + i + " ";
+		}
+		return str + "\n";
+	}
 
-    public void setNblignes(int i) {
-        this.nbLignes = i;
-    }
+	public String caseVide() {
+		return "[ ]";
+	}
 
-    public void setNbColonnes(int i) {
-        this.nbColonnes = i;
-    }
-    
-	public boolean isDecors(Position laPosition) {
+	public String afficherLigne(final int i) {
+		String retour = "" + i;
+		for (int x = 0; x < this.nbColonnes; x++) {
+			Position courante = new Position(x, i);
+
+			boolean aAfficher = false;
+			for (Elements objet : this.objets) {
+				if (objet.getPosition().equals(courante)) {
+					retour += objet.afficher();
+					aAfficher = true;
+				}
+			}
+			if (!aAfficher) {
+				retour += caseVide();
+			}
+		}
+		return retour + "\n";
+	}
+
+	public List<Elements> getElements() {
+		return this.objets;
+	}
+
+	public void setNblignes(final int i) {
+		this.nbLignes = i;
+	}
+
+	public void setNbColonnes(final int i) {
+		this.nbColonnes = i;
+	}
+
+	public boolean isDecors(final Position laPosition) {
 
 		for (Elements element : this.objets) {
-			if (laPosition.equals( element.getPosition())) {
+			if (laPosition.equals(element.getPosition())) {
 				return element instanceof Decors;
 			}
 		}
 		return false;
 	}
-        
-        public boolean isDansLaGrille(Position laPosition){
-            if(laPosition.getX() > this.getNbColonnes())
-                return false;
-            if(laPosition.getY() > this.getNbLignes())
-                return false;
-            if(laPosition.getX() < 0)
-                return false;
-            if(laPosition.getY() < 0)
-                return false;
-            return true;
-        }
-    	
-    	public boolean isValide(Position laPosition){
-    		
-    	 boolean valide = true;
-    	 valide = (isDansLaGrille(laPosition));
-    	 valide = (!isDecors(laPosition));
-         
-    	 
-    	 return valide;
-    	
-    	
-    }
-    	
-    
-    
-    
+
+	public boolean isDansLaGrille(final Position laPosition) {
+		if (laPosition.getX() > getNbColonnes()) {
+			return false;
+		}
+		if (laPosition.getY() > getNbLignes()) {
+			return false;
+		}
+		if (laPosition.getX() < 0) {
+			return false;
+		}
+		if (laPosition.getY() < 0) {
+			return false;
+		}
+		return true;
+	}
+
+	public boolean isValide(final Position laPosition) {
+
+		boolean valide = true;
+		valide = isDansLaGrille(laPosition);
+		valide = !isDecors(laPosition);
+
+		return valide;
+
+	}
+
 }
--- test/consolefps/models/elements/bonus/MunitionsTest.java
+++ test/consolefps/models/elements/bonus/MunitionsTest.java
@@ -4,50 +4,31 @@
  */
 package consolefps.models.elements.bonus;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
- *
+ * 
  * @author SpectroDT
  */
 public class MunitionsTest {
-    
-    public MunitionsTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
 
-    /**
-     * Test of afficher method, of class Munitions.
-     */
-    @Test
-    public void testAfficher() {
-        //Arrange
-        Munitions ammo = new Munitions(1,2);
-        
-        //Act
-        String s = ammo.afficher();
-        
-        //Assert 
-        assertEquals("[m]", s);
-    }
+	public MunitionsTest() {
+	}
+
+	/**
+	 * Test of afficher method, of class Munitions.
+	 */
+	@Test
+	public void testAfficher() {
+		// Arrange
+		Munitions ammo = new Munitions(1, 2);
+
+		// Act
+		String s = ammo.afficher();
+
+		// Assert
+		assertEquals("[m]", s);
+	}
 }
